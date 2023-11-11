4e4113bdf66e6d6ef5e763ebe3b0a5c9b219def6
chiragfalor <cfalor@mit.edu>
2023-11-11 16:40:32 -0500

    interface try 1

---
+++ first_pipeline/data_load.py
@@ -0,0 +1,18 @@
+
+import json
+from GameModel import GameModel
+
+def load_games():
+    with open('data/game_desc.json') as f:
+        data = json.load(f)
+
+    games = []
+    for game in data['game_descriptions']:
+        game_code_path = game['ppl_code_file_path']
+        # load description
+        with open(f'data/{game_code_path}') as f:
+            code = f.read()
+        games.append(GameModel(game['name'], game['description'], code))
+        
+    return games
+
