435810aaf37597769928ed2fc5939bdccc80b744
chiragfalor <cfalor@mit.edu>
2023-11-23 15:00:07 -0500

    basic model testing

--- first_pipeline/GameModel.py
+++ first_pipeline/GameModel.py
@@ -1,7 +1,14 @@
 class TextToCode:
-    def __init__(self, description, ppl_code) -> None:
-        self.text = description
+    def __init__(self, text, ppl_code) -> None:
+        self.text = text
         self.code = ppl_code
+
+    @property
+    def code(self):
+        return self._code
+    @code.setter
+    def code(self, value):
+        self._code = value
 
     def __repr__(self) -> str:
         return f"Text: {self.text}\nCode: {self.code}"
@@ -10,9 +17,16 @@ class GameModel(TextToCode):
     def __init__(self, name, description, ppl_code) -> None:
         super().__init__(description, ppl_code)
         self.name = name
+    
+    @property
+    def description(self):
+        return self.text
+    @description.setter
+    def description(self, value):
+        self.text = value
 
     def __repr__(self) -> str:
-        return f"Name: {self.name}\nDescription: {self.text}\nCode: {self.code}"
+        return f"Name: {self.name}\nDescription: {self.description}\nCode: {self.code}"
 
 class GameEvent(TextToCode):
     def __init__(self, description, ppl_code) -> None:
--- first_pipeline/data_load.py
+++ first_pipeline/data_load.py
@@ -16,3 +16,8 @@ def load_games():
         
     return games
 
+def load_system_prompt():
+    with open('system_prompt.txt') as f:
+        system_prompt = f.read()
+    return system_prompt
+
