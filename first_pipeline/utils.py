65e9335c6974223c4ee51e42dae5d6c0a1c50e9b
chiragfalor <cfalor@mit.edu>
2023-11-28 17:16:35 -0500

    inputting fn description

--- first_pipeline/utils.py
+++ first_pipeline/utils.py
@@ -22,6 +22,10 @@ def get_model_response(message, gpt_model=GPT_model, random_seed=RANDOM_SEED):
     )
     return completion.choices[0].message.content
 
+def get_fn_defn(code):
+    code = code.split("//---------------")
+    return code[0]
+
 def get_message_with_example(game_question, game_examples, system_prompt_file='new_system_prompt.txt'):
     messages = []
     
@@ -34,6 +38,12 @@ def get_message_with_example(game_question, game_examples, system_prompt_file='n
 
     # final prompt
     messages.append({"role": "user", "content": f"Name: {game_question.name}\nDescription: {game_question.description}"})
+    return messages
+
+def get_message_with_example_and_function_defn(game_question, game_examples, system_prompt_file='new_system_prompt.txt'):
+    messages = get_message_with_example(game_question, game_examples, system_prompt_file)
+    fn_description = get_fn_defn(game_question.code)
+    messages.append({"role": "assistant", "content": fn_description})
     return messages
 
 
